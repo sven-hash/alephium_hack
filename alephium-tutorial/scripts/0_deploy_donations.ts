09db187c7f678a6c2563431bc0672e7a400746e8
cgi-bin/ <6095048+sven-hash@users.noreply.github.com>
2024-09-05 17:45:05 +0200

    Update 0_deploy_donations.ts

--- alephium-tutorial/scripts/0_deploy_donations.ts
+++ alephium-tutorial/scripts/0_deploy_donations.ts
@@ -1,8 +1,6 @@
 import { Deployer, DeployFunction, Network } from '@alephium/cli'
 import { Settings } from '../alephium.config'
 import { Donations } from '../artifacts/ts'
-import { CallContractParams } from '@alephium/web3'
-import { testNodeWallet } from '@alephium/web3-test'
 
 const deployFaucet: DeployFunction<Settings> = async (
   deployer: Deployer,
@@ -18,20 +16,6 @@ const deployFaucet: DeployFunction<Settings> = async (
   })
   console.log('Token faucet contract id: ' + result.contractInstance.contractId)
   console.log('Token faucet contract address: ' + result.contractInstance.address)
-
-  const signer = await testNodeWallet()
-  const addresses = await signer.getAccounts()
-  const address = addresses[0]
-
-  const params: CallContractParams<{ donor: string }> = {
-    args: { donor: address.address }
-  };
-
-  const contract = result.contractInstance
-  const getDonationBal = await contract.view.getDonorTotal(params);
-
-  console.log(getDonationBal.returns)
 }
 
 export default deployFaucet
-
