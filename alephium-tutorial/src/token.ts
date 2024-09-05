5fdbec3b09b949dd83632be5306f48ac9a21103a
cgi-bin/ <6095048+sven-hash@users.noreply.github.com>
2024-09-05 17:45:56 +0200

    Update token.ts

--- alephium-tutorial/src/token.ts
+++ alephium-tutorial/src/token.ts
@@ -1,48 +1,37 @@
 import { Deployments } from '@alephium/cli'
-import { web3, DUST_AMOUNT } from '@alephium/web3'
+import { web3, CallContractParams, MINIMAL_CONTRACT_DEPOSIT } from '@alephium/web3'
 import { testNodeWallet } from '@alephium/web3-test'
 import configuration from '../alephium.config'
-import { TokenFaucet } from '../artifacts/ts'
+import { Donations } from '../artifacts/ts'
 
 async function withdraw() {
-  web3.setCurrentNodeProvider('http://127.0.0.1:22973')
-
-  // Attention: test wallet is used for demonstration purpose
   const signer = await testNodeWallet()
+  const addresses = await signer.getAccounts()
+  const address = addresses[0]
+
+  web3.setCurrentNodeProvider('http://127.0.0.1:22973')
 
   const deployments = await Deployments.load(configuration, 'devnet')
 
-  // The test wallet has four accounts with one in each address group
-  // The wallet calls withdraw function for all of the address groups
-  for (const account of await signer.getAccounts()) {
-    // Set an active account to prepare and sign transactions
-    await signer.setSelectedAccount(account.address)
-    const accountGroup = account.group
-
-    // Load the metadata of the deployed contract in the right group
-    const faucet = deployments.getInstance(TokenFaucet, accountGroup)
-    if (faucet === undefined) {
-      console.log(`The contract is not deployed on group ${account.group}`)
-      continue
-    }
-    const tokenId = faucet.contractId
-    const tokenAddress = faucet.address
-    console.log(`Token faucet contract id: ${tokenId}`)
-    console.log(`Token faucet contract address: ${tokenAddress}`)
-
-    // Submit a transaction to use the transaction script
-    await faucet.transact.withdraw({
-      signer: signer,
-      attoAlphAmount: DUST_AMOUNT * 2n,
-      args: {
-        amount: 1n
-      },
-    })
-
-    // Fetch the latest state of the token contract
-    const state = await faucet.fetchState()
-    console.log(JSON.stringify(state.fields, null, '  '))
+  const params: CallContractParams<{ donor: string }> = {
+    args: { donor: address.address }
   }
+
+  const contract = deployments.getInstance(Donations, address.group)
+
+  // deposit to the user
+  await contract?.transact.depositToUser({
+    args: {
+      recipient: address.address,
+      amount: 1n
+    },
+    signer: signer,
+    attoAlphAmount: MINIMAL_CONTRACT_DEPOSIT // APS, need to approve the amount for the map
+  })
+
+  // check the donation
+  const getDonationBal = await contract?.view.getDonorTotal(params)
+  console.log(getDonationBal?.returns)
 }
 
 withdraw()
